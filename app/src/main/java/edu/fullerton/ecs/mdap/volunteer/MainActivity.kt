d18469aaf0beae6201c3b24ff9bfc336fcc579fb
Paul Inventado <pinventado@fullerton.edu>
2021-09-22 09:46:14 -0700

    Initial commit

---
+++ app/src/main/java/edu/fullerton/ecs/mdap/volunteer/MainActivity.kt
@@ -0,0 +1,19 @@
+package edu.fullerton.ecs.mdap.volunteer
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        // TODO: Setup data binding for activity_main.xml.
+
+        // TODO: Create a mutableList to hold volunteer name data
+
+        // TODO: Create a RecyclerView adapter object and assign it to the RecyclerView.
+
+        // TODO: Handle onClick events on the "Add Volunteer" button.
+
+        // TODO: Add the volunteer to the mutableList and notify the RecyclerView about the change.
+    }
+}
---
+++ app/src/main/java/edu/fullerton/ecs/mdap/volunteer/adapter/VolunteerRVAdapter.kt
@@ -0,0 +1,22 @@
+package edu.fullerton.ecs.mdap.volunteer.adapter
+
+/**
+ * TODO: Create a RecyclerView adapter. Provide the necessary parameters, make sure it inherits from
+ * the correct class and use the appropriate type parameters.
+ */
+class VolunteerRVAdapter {
+
+    /**
+     *  TODO: Create an inner class that will hold the view. Make sure it inherits from the correct
+     *  class. Provide properties that will store references to the view elements. Connect the view
+     *  elements to the properties.
+     */
+
+    /**
+     * TODO: Implement the three required methods for a RecyclerView adapter. One method is
+     * responsible for creating the view holder object and passing its necessary arguments, another
+     * method will assign the values from the data to the view holder's view elements, adnd the
+     * third returns the number of elements in the data set.
+     */
+
+}
