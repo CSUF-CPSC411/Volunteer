e5eb1e2d15425c87a7092e97369ad94ad4d88d4e
Paul Inventado <pinventado@fullerton.edu>
2021-09-22 12:12:24 -0700

    Create solution

--- app/src/main/java/edu/fullerton/ecs/mdap/volunteer/adapter/VolunteerRVAdapter.kt
+++ app/src/main/java/edu/fullerton/ecs/mdap/volunteer/adapter/VolunteerRVAdapter.kt
@@ -1,10 +1,27 @@
 package edu.fullerton.ecs.mdap.volunteer.adapter
 
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import edu.fullerton.ecs.mdap.volunteer.R
+
 /**
  * TODO: Create a RecyclerView adapter. Provide the necessary parameters, make sure it inherits from
  * the correct class and use the appropriate type parameters.
  */
-class VolunteerRVAdapter {
+class VolunteerRVAdapter(
+    var parent: Context,
+    var data: MutableList<String> = mutableListOf<String>()
+) :
+    RecyclerView.Adapter<VolunteerRVAdapter.VolunteerHolder>() {
+
+    class VolunteerHolder(view: View) : RecyclerView.ViewHolder(view) {
+        var name: TextView = view.findViewById(R.id.name)
+    }
+
 
     /**
      *  TODO: Create an inner class that will hold the view. Make sure it inherits from the correct
@@ -12,11 +29,26 @@ class VolunteerRVAdapter {
      *  elements to the properties.
      */
 
+
     /**
      * TODO: Implement the three required methods for a RecyclerView adapter. One method is
      * responsible for creating the view holder object and passing its necessary arguments, another
      * method will assign the values from the data to the view holder's view elements, adnd the
      * third returns the number of elements in the data set.
      */
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): VolunteerHolder {
+        var layout = LayoutInflater.from(parent.context)
+        var view = layout.inflate(R.layout.volunteer_list_item, parent, false)
+        return VolunteerHolder(view)
+    }
+
+    override fun onBindViewHolder(holder: VolunteerHolder, position: Int) {
+        holder.name.text = data[position]
+    }
+
+    override fun getItemCount(): Int {
+        return data.size
+    }
 
 }
